2781cadb90f7a042c16d95ffe7e8b193abd690b3
THAIDTC <thaidtc@MacBook-Pro-cua-THAIDTC.local>
2026-05-28 10:01:56 +0700

    feat: authentication with login/logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $data['email'])->first();
+
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['message' => 'Login successful', 'access_token' => $token, 'token_type' => 'Bearer'], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out successfully'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API is working'], 200);
@@ -13,4 +14,13 @@ Route::get('/test', function () {
 // Route::put('/todos/{id}', [TodoController::class, 'update']);
 // Route::delete('/todos/{id}', [TodoController::class, 'destroy']);
 
-Route::apiResource('todos', TodoController::class)->only(['index', 'show', 'store', 'update', 'destroy']);
+// Route::apiResource('todos', TodoController::class)->only(['index', 'show', 'store', 'update', 'destroy']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+// check apis bằng laravel sanctum
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('todos', TodoController::class)->only(['index', 'show', 'store', 'update', 'destroy']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
